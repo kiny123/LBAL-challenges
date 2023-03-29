8e2f984b5fb86137a06174628d505527d4a49128
kiny123 <89192734912nik@gmail.com>
2023-03-30 01:04:20 +0300

    init

---
+++ Challenge1/Challenge1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Challenge1
+//
+//  Created by nikita on 30.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
