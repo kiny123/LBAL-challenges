63809b6e4856db4f1f9335d10d4c25b37ca4152f
kiny123 <89192734912nik@gmail.com>
2023-03-30 23:41:46 +0300

    Challenge 4

---
+++ Challenge 4/Challenge 4/RGBView.swift
@@ -0,0 +1,52 @@
+//
+//  RGBView.swift
+//  Challenge 4
+//
+//  Created by nikita on 30.03.2023.
+//
+
+import UIKit
+
+@IBDesignable
+class RGBView: UIView {
+
+    var viewsArray = [UIView]()
+
+    @IBInspectable
+    var spacing: CGFloat = 20 {
+        didSet {
+            setNeedsLayout()
+        }
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupView()
+    }
+
+    private func setupView()  {
+        let colors = [UIColor.red, UIColor.green, UIColor.blue]
+        for color in colors {
+            let view = UIView()
+            view.backgroundColor = color
+            addSubview(view)
+            viewsArray.append(view)
+        }
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        let barHeight = (bounds.height - (spacing * (CGFloat(viewsArray.count - 1)))) / CGFloat(viewsArray.count)
+        var yShift: CGFloat = 0
+        for view in viewsArray {
+            view.frame = CGRect(x: 0, y: yShift, width: bounds.width, height: barHeight)
+            yShift += barHeight + spacing
+
+        }
+    }
+}
